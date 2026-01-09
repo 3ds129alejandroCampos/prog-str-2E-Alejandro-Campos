f401b0bab7def89671ccdaca5de33849596d42b4
Alejandro Taboada <20253ds129@utez.edu.mx>
2026-01-09 13:44:41 -0600

    completo

--- HolaMundo01/src/Main.java
+++ HolaMundo01/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
+
+        System.out.println("Alejandro Campos Taboada");
+        System.out.println("Grupo: 2E");
+        System.out.println("09/01/2026");
         System.out.println("Hola Mundo");
+
     }
 }
