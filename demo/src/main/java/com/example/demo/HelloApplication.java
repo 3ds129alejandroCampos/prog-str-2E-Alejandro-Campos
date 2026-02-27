2a4db67ad86418dc88c71c1f381b49519c4f0cc0
Alejandro Taboada <20253ds129@utez.edu.mx>
2026-02-26 23:18:13 -0600

    error al visualisar la scene 2 corregido

---
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        //crea ventana
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("home-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.controllers;
+    opens com.example.demo.controllers to javafx.fxml;
+}
